493d7f042ffad4428ea052cd53ff1d89e8b8c691
miguelcarvalho <miguelvdscarvalho@gmail.com>
2024-04-22 14:53:11 +0100

    Paginas feitas necessario criar modelos

--- Modulos/database.py
+++ Modulos/database.py
@@ -20,7 +20,6 @@ def check_if_user_exists(username,password):
         query = sql.text(f"SELECT id FROM users WHERE name = '{username}'")
         result = conn.execute(query)
         data = result.fetchall()
-        print(data[0][0])
         if len(data)==0:
             return False
         else:
@@ -66,3 +65,61 @@ def check_pass(id,password):
             return False
     
     
+def modify_estado(id):
+    engine = sql.create_engine('postgresql://postgres:admin@localhost/frontend')
+    with engine.connect() as conn:
+        query = sql.text("UPDATE users SET estado = 0 WHERE id = :id")
+        params = {'id': id}
+        result = conn.execute(query, params)
+        conn.commit()
+        
+        
+def select_from_table(table):
+    engine = sql.create_engine('postgresql://postgres:admin@localhost/frontend')
+    with engine.connect() as conn:
+        query = sql.text(f"SELECT * FROM {table}")
+        result = conn.execute(query)
+        columns = result.keys()
+        data = result.fetchall()
+        return columns, data
+    
+def store_dataset(dataset,name,tipo):
+    engine = sql.create_engine('postgresql://postgres:admin@localhost/frontend')
+    with engine.connect() as conn:
+        try:
+            df = pd.read_csv(dataset)
+            df.drop(columns=['Unemployment rate', 'Inflation rate', 'GDP'], inplace=True)
+            
+            query = sql.text("INSERT INTO dataset (name, tipo) VALUES (:name, :tipo) RETURNING id_dataset")
+            params = {'name': name, 'tipo': tipo}
+            result = conn.execute(query, params)
+            id = result.fetchone()[0]
+            
+            # Add a new column to the DataFrame with the ID
+            df['id_dataset'] = id
+            
+            # Insert the DataFrame into the 'dataset_atributos' table
+            df.to_sql('dataset_atributos', conn, if_exists='append', index=False)
+            
+            conn.commit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            
+            
+def select_head_dataset(id):
+    engine = sql.create_engine('postgresql://postgres:admin@localhost/frontend')
+    with engine.connect() as conn:
+        query = sql.text(f"SELECT * FROM dataset_atributos WHERE id_dataset={id} LIMIT 5")
+        result = conn.execute(query)
+        columns = result.keys()
+        data = result.fetchall()
+        return data,columns
+    
+def select_from_table_id_one_dataset(table,id):
+    engine = sql.create_engine('postgresql://postgres:admin@localhost/frontend')
+    with engine.connect() as conn:
+        query = sql.text(f"SELECT * FROM {table} WHERE id_dataset = {id}")
+        result = conn.execute(query)
+        columns = result.keys()
+        data = result.fetchall()
+        return data,columns
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, redirect, render_template, request, url_for
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
 import sqlalchemy
 
-from Modulos.database import check_if_user_exists, check_pass, login_query, register_user
+from Modulos.database import change_password, check_if_user_exists, check_pass, login_query, register_user, select_from_table, select_from_table_id_one_dataset, select_head_dataset, store_dataset
+from Modulos.database import modify_estado
 
 app = Flask(__name__)
 
@@ -26,12 +27,10 @@ def user_loader(id):
     return user
 
 
-    
-
 @app.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html',user_type=current_user.tipo)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -46,10 +45,11 @@ def login():
         if idd!=False and check_pass(idd,password):
             user=user_loader(idd)
             login_user(user)
-            return redirect(url_for('index'))
+            return render_template('index.html',user_type=current_user.tipo)
     return render_template('login.html')
 
 @app.route('/handle_register', methods=['GET', 'POST'])
+@login_required
 def handle_register():
     name = request.form['name']
     email= request.form['email']
@@ -58,28 +58,105 @@ def handle_register():
     cargo = request.form['cargo']
     if(password==confirmpassword):
         if register_user(name,email,password,cargo):
-            return redirect(url_for('login'))
-    return redirect(url_for('register'))
+            return render_template('login.html',user_type=current_user.tipo)
+    return render_template('register.html',user_type=current_user.tipo)
     
         
     
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/register')
+@login_required
 def register():
-    return render_template('register.html')
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('home'))
+    return render_template('register.html',user_type=current_user.tipo)
 
-@app.route('/desativar')
+@app.route('/desativar',methods=['GET', 'POST'])
+@login_required
 def desativar():
-    engine = sql.create_engine('postgresql://postgres:admin@localhost/frontend')
-    with engine.connect() as conn:
-        result = conn.execute("SELECT * FROM your_table")
-        columns = result.keys()
-        data = result.fetchall()
-    return render_template('desativar.html', columns=columns, data=data)
+    if request.method == 'POST':
+        selected_ids = request.args.getlist('user_id')
+        for id in selected_ids:
+            modify_estado(id)
+        return render_template('desativar.html',user_type=current_user.tipo)
+    elif request.method == 'GET':
+        columns, data = select_from_table('users')
+        return render_template('desativar.html', columns=columns, data=data,user_type=current_user.tipo)
 
+@app.route('/changepw')
+@login_required
+def changepw():
+    return render_template('changepw.html',user_type=current_user.tipo)
+    
+@app.route('/changepass', methods=['GET', 'POST'])
+@login_required
+def changepass():
+    id = current_user.id
+    password = request.form.get('password')
+    confirmpassword = request.form.get('confirmpassword')
+    if password and confirmpassword and password == confirmpassword:
+        if change_password(id, password):
+            return render_template('changepw.html',user_type=current_user.tipo)
+        else:
+            return 'An error occurred'
+    
+
+@app.route('/admin')
+@login_required
+def admin():
+    return render_template('admin.html',user_type=current_user.tipo)
+
+@app.route('/conta')
+@login_required
+def conta():
+    return render_template('conta.html',user_type=current_user.tipo)
+
+@app.route('/dados_inicial')
+@login_required
+def dados_inicial():
+    return render_template('dados_inicial.html',user_type=current_user.tipo)
+
+@app.route('/modelar')
+@login_required
+def modelar():
+    return render_template('modelar.html',user_type=current_user.tipo)
+
+
+@app.route('/dados_upload_file', methods=['GET', 'POST'])
+@login_required
+def dados_upload_file():
+    if request.method=="GET":
+        return render_template('dados_upload_file.html',user_type=current_user.tipo)
+    elif request.method=="POST":
+        file = request.files['fileda']
+        name = request.form['nome']
+        type = request.form['tipo']
+        print(file,name,type)
+        store_dataset(file,name,type)
+        return render_template('dados_upload_file.html',user_type=current_user.tipo)
+    
+    
+@app.route('/dados_select_dataset', methods=['GET'])
+@login_required
+def dados_select_dataset():
+        columns, data = select_from_table('dataset')
+        return render_template('dados_select_dataset.html', columns=columns, data=data,user_type=current_user.tipo)
+    
+@app.route('/show_dataset', methods=['GET', 'POST'])
+@login_required
+def show_dataset():
+    if request.method=="POST":
+        id_dataset = request.form['id_dataset']
+        if id_dataset:
+            data,columns = select_from_table_id_one_dataset('dataset',id_dataset)
+            data_five,columns_five=select_head_dataset(id_dataset)
+            lengc=len(columns_five)
+            dd,ccc = select_from_table_id_one_dataset('dataset_atributos',id_dataset)
+            lengd=len(dd)
+            return render_template('show_dataset.html',user_type=current_user.tipo,data=data,columns=columns,data_five=data_five,columns_five=columns_five,lengc=lengc,lengd=lengd)
+        return render_template('index.html',user_type=current_user.tipo)
 if __name__ == '__main__':
     app.run()
